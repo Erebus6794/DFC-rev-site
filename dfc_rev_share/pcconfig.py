0236f77b8731415de32c5ae85359be789deb9feb
Erebus6794 <110900737+Erebus6794@users.noreply.github.com>
2023-05-02 19:24:48 +1000

    Update pcconfig.py

--- dfc_rev_share/pcconfig.py
+++ dfc_rev_share/pcconfig.py
@@ -2,6 +2,8 @@ import pynecone as pc
 
 config = pc.Config(
     app_name="dfc_rev_share",
+    api_url="http://dfcrevenue.com:8000",
+    bun_path="$HOME/.bun/bin/bun",
     db_url="sqlite:///pynecone.db",
     env=pc.Env.DEV,
 )
